903d4a42d0dd4ba08abd626cafb18e2b4f75c9e0
LeeHyunsoo <mm083434@gmail.com>
2023-10-12 22:05:03 +0900

    Initial Commit

---
+++ Test-ScreenTransaction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test-ScreenTransaction
+//
+//  Created by HyunSoo on 2023/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
